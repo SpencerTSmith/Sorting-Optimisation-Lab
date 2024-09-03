7e170e557e57e38b531114c7562a40e8213b78f3
SpencerTSmith <spencertsmith2003@gmail.com>
2024-09-02 22:19:39 -0500

    clean up

--- src/test_var001.c
+++ src/test_var001.c
@@ -1,10 +1,3 @@
-/*
-  This is a simple implementation of an unoptimized sort.
-
-  - richard.m.veras@ou.edu
-
-*/
-
 #include <stdbool.h>
 
 #ifndef COMPUTE_NAME
@@ -12,25 +5,13 @@
 #endif
 
 void COMPUTE_NAME(int m0, float *x, float *y) {
-	/*
-	  NOTE: This is just a copy of the baseline. You need to modify
-	  and iterate on this.
-	*/
-
-	/*
-	   We need an out-of-place sort (input array != output array)
-	   so we will do the slowest but easiest thing to get a working
-	   implementation. First we will copy the contents of the input
-	   array x into array y, then perform an in-place sort on y.
-
-	*/
 
 	// copy the contents of the input array into the output array
 	for (int i0 = 0; i0 < m0; ++i0)
 		y[i0] = x[i0];
 
-	// perform the absolute worse in-place bubble sort.
 	for (int i = 0; i < m0; ++i) {
+		// if we haven't swapped we know it's sorted
 		bool swapped = false;
 		for (int j = 0; j < m0 - 1; ++j) {
 			if (y[j] > y[j + 1]) {
